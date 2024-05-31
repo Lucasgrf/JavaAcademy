d2b2ffdaeea3d5164edb448aa9cc454b4725849b
Lucasgrf <lucasgabrielrf.contato@gmail.com>
2024-05-31 19:00:28 -0300

    .

--- src/main/java/aula01/HelloWorld.java
+++ src/main/java/aula01/HelloWorld.java
@@ -1,9 +1,7 @@
+package aula01;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        for(int i = 0; i <= 5; i++){
-            System.out.println("i = " + i);
-        }
     }
 }
