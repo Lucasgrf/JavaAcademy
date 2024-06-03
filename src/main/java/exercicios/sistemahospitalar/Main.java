d5bef9fbb056e3df5c35e04fda782b15833d755a
Lucasgrf <lucasgabrielrf.contato@gmail.com>
2024-06-03 03:50:08 -0300

    novos exercicios

---
+++ src/main/java/exercicios/sistemahospitalar/Main.java
@@ -0,0 +1,16 @@
+package exercicios.sistemahospitalar;
+
+public class Main {
+    public static void main(String[] args) {
+        Paciente paciente = new Paciente("Lucas", 55.3,1.69);
+        Paciente paciente2 = new Paciente("Wesley", 120,1.60);
+        Paciente paciente3 = new Paciente("Matheus", 83.2,1.93);
+
+        System.out.println(paciente.diagnostico());
+        System.out.println(paciente2.diagnostico());
+        System.out.println(paciente3.diagnostico());
+
+    }
+
+
+}
