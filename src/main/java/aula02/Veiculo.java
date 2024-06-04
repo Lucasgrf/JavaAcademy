7dd46cb596d4d71923f5a13068fac4d57a555b90
Lucasgrf <lucasgabrielrf.contato@gmail.com>
2024-06-03 22:01:59 -0300

    nova aula.

---
+++ src/main/java/aula02/Veiculo.java
@@ -0,0 +1,14 @@
+package aula02;
+
+public class Veiculo {
+    public int numeroRodas;
+    public String tipoPropulsao;
+
+    @Override
+    public String toString() {
+        return "Veiculo{" +
+                "numeroRodas=" + numeroRodas +
+                ", tipoPropulsao='" + tipoPropulsao + '\'' +
+                '}';
+    }
+}
