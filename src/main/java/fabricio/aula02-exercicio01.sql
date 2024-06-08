399e7e692b37f685aa80d23d4bfee3559728d9eb
Lucas <lucasgabrielrf.contato@gmail.com>
2024-06-07 21:12:50 -0300

    .

--- src/main/java/fabricio/aula02-exercicio01.sql
+++ src/main/java/fabricio/aula02-exercicio01.sql
@@ -4,7 +4,7 @@ drop table tipo_evento
 create table tipo_evento
 (
 	id serial primary key,
-	tipo varchar(60)
+	tipo varchar(60) not null
 )
 
 create table pessoas
@@ -12,17 +12,17 @@ create table pessoas
 	cpf varchar(11) primary key,
 	nome varchar(120) not null,
 	email varchar(150) not null,
-	fone varchar(20) not null
+	fone varchar(20)
 )
 
 create table eventos 
 (
 	id serial primary key,
 	nome varchar(120) not null,
-	data_init date not null,
-	data_fim date not null,
-	localizacao varchar(150),
-	fk_tipo integer not null,
+	data_init date,
+	data_fim date,
+	localizacao varchar(150) not null,
+	fk_tipo integer,
 	foreign key (fk_tipo) references tipo_evento(id)
 )
 
@@ -30,12 +30,12 @@ create table programacao
 (
 	id serial primary key,
 	nome varchar(120) not null,
-	data_init date not null,
-	data_fim date not null,
-	descricao varchar(1000) not null,
+	data_init date,
+	data_fim date,
+	descricao varchar(1000),
 	lugar varchar(120) not null,
 	responsavel varchar(100) not null,
-	fk_evento integer not null,
+	fk_evento integer,
 	foreign key (fk_evento) references eventos(id)
 	
 )
@@ -46,16 +46,16 @@ create table ingressos
 	titulo varchar(100) not null,
 	lugar varchar(120) not null,
 	valor real not null,
-	quantidade integer not null,
-	fk_evento integer not null,
+	quantidade integer,
+	fk_evento integer,
 	foreign key (fk_evento) references eventos(id)
 )
 
 create table compras
 (
 	id serial primary key,
-	quantidade integer not null,
-	fk_pessoa varchar(11) not null,
+	quantidade integer,
+	fk_pessoa varchar(11),
 	foreign key (fk_pessoa) references pessoas(cpf)
 )
 
